9501c3d7fa4667f5cedb5f2f5e3149f34dcad60c
Nikita Ovchinnikov <n.ovchinnikov96@yandex.ru>
2022-11-30 14:02:10 +0300

    Wikipedia_Parsing_Bot
    Wikipedia_Parsing_Bot

---
+++ app.py
@@ -0,0 +1,32 @@
+from aiogram import Bot, Dispatcher, executor, types
+from config import AllConfigurations
+from parser_url import ParserUrl
+
+import logging
+
+# инициализация бота
+bot = Bot(token=AllConfigurations.TOKEN)
+dp = Dispatcher(bot)
+
+# логирование в файл py_log.log в режиме перезаписи при каждом запуске бота с указанием времени
+logging.basicConfig(level=logging.INFO, filename='py_log.log', filemode='w', format='%(asctime)s %(levelname)s %(message)s')
+
+# обработка команды /start
+@dp.message_handler(commands=['start'])
+async def send_welcome(message: types.Message):
+    logging.info(f'Пользователь {message.from_user.full_name} активировал бота')
+    await bot.send_message(message.from_user.id, AllConfigurations.welcome_text)
+    logging.info(f'Бот отправил приветственное сообщение пользователю {message.from_user.full_name}')
+
+# обработка ввода текста пользователем
+@dp.message_handler(content_types=['text'])
+async def send_wiki(message: types.Message):
+    logging.info(f'Пользователь {message.from_user.full_name} ввёл сообщение {message.text}')
+    # итоговый запрос пользователя, который состоит из заданной ссылки на сайт + введенный текст
+    user_request = ParserUrl.url + message.text
+    await bot.send_message(message.from_user.id, AllConfigurations.link_to_website)
+    await bot.send_message(message.from_user.id, f'<a href="{user_request}">Нажимай</a>', parse_mode='HTML')
+    logging.info(f'Полльзователь {message.from_user.full_name} получил сообщение со ссылкой {user_request}')
+
+# бот не отвечает на сообщения, которые были отправлены, когда бот был оффлайн
+executor.start_polling(dp, skip_updates=True)
---
+++ config.py
@@ -0,0 +1,12 @@
+import emoji
+
+class AllConfigurations():
+    TOKEN = '5860379795:AAFy9vSpIa31sZ18uabq0CUuyx9xFGaJ0QE'
+
+    welcome_text = emoji.emojize('Привет!:wave: Я бот.\
+    \n\nЯ умею присылать интересные статьи из Wikipedia.:smiley:\
+    \nОтправь мне любое слово и я подберу для тебя статью.:wink:', language='alias')
+
+    link_to_website = emoji.emojize(':tada::tada::tada::tada::tada::tada:\
+        \nПохоже, что ты искал вот эту статью :point_down: Если нет, то проверь, верно ли ты написал свой запрос\
+        \nМожет быть, ты написал какую-то абракадабру. :crystal_ball:', language='alias')
